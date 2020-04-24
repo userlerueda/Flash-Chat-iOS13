37bfa9598e2b0c011916ee0dff1273b1930d06e0
Luis Rueda <lurueda@cisco.com>
2020-04-23 22:52:20 -0500

    [feat] add functionality up to 202 - adds CLTypingLabel - Add FireBase initial configuration - Add Constants (K) - Add functionality for Log Out - Add UITableViewDataSource - Add functionality for loginPressed - Add functionality for registerPressed - Add message structure - add .gitignore - add MessageCell Cocoa Pod

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Luis Rueda on 24/04/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Luis Rueda on 24/04/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
